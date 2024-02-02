8c23e006706de9e8713db4cfc02657f0482795ff
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-02-02 09:07:20 -0500

    Initial Commit

---
+++ ArithmeticBuddy/ArithmeticBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticBuddyApp.swift
+//  ArithmeticBuddy
+//
+//  Created by Nikita Zhdanov on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
