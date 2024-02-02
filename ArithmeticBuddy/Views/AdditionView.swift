7b1cec96e0b0845fde6c70b6355671cf008e9498
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-02-02 09:08:50 -0500

    Created the project and finished initial project organization.

---
+++ ArithmeticBuddy/Views/AdditionView.swift
@@ -0,0 +1,18 @@
+//
+//  AdditionView.swift
+//  ArithmeticBuddy
+//
+//  Created by Nikita Zhdanov on 2024-02-02.
+//
+
+import SwiftUI
+
+struct AdditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdditionView()
+}
